a6e2e8a452c306fbb3d2a56697064adbb6741707
saranshbarua <saranshbarua@gmail.com>
2019-03-13 12:18:51 +0530

    add image

--- src/components/LandingPage/landingPage.jsx
+++ src/components/LandingPage/landingPage.jsx
@@ -6,20 +6,21 @@ const LandingPage = () => {
   return (
     <section className="lp-sec">
       <div className="lp-image">
-        {/* <img
-          src="https://worldofcricket.in/wp-content/uploads/2018/10/wallpaper2you_253926.jpg"
+        <img
+          src="http://www.mobileswall.com/wp-content/uploads/2015/01/640-Sachin-Tendulkar-God-of-Cricket-l.jpg"
           alt="Sachin's poster"
-          height="80%"
-          width="80%"
-        /> */}
+          height="85%"
+          width="auto"
+          className="img-card"
+        />
       </div>
       <div className="lp-content">
         <Fade bottom delay={1000}>
-          {/* <span className="pd-700 name">Sachin</span> */}
+          <span className="pd-700 name">Sachin</span>
         </Fade>
         <br />
         <Fade bottom delay={8000}>
-          {/* <span className="pd-700 name">Tendulkar</span> */}
+          <span className="pd-700 name">Tendulkar</span>
         </Fade>
       </div>
     </section>
