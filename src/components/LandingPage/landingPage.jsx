a6faccf4f0b2040ab20f68d893fbe2d184847426
saranshbarua <saranshbarua@gmail.com>
2019-03-13 13:15:57 +0530

    Add basic landing page

--- src/components/LandingPage/landingPage.jsx
+++ src/components/LandingPage/landingPage.jsx
@@ -6,21 +6,32 @@ const LandingPage = () => {
   return (
     <section className="lp-sec">
       <div className="lp-image">
-        <img
-          src="http://www.mobileswall.com/wp-content/uploads/2015/01/640-Sachin-Tendulkar-God-of-Cricket-l.jpg"
-          alt="Sachin's poster"
-          height="85%"
-          width="auto"
-          className="img-card"
-        />
+        <Fade>
+          <img
+            src="http://www.mobileswall.com/wp-content/uploads/2015/01/640-Sachin-Tendulkar-God-of-Cricket-l.jpg"
+            alt="Sachin's poster"
+            className="img-card"
+          />
+        </Fade>
       </div>
       <div className="lp-content">
         <Fade bottom delay={1000}>
-          <span className="pd-700 name">Sachin</span>
+          <span className="pd-700 name">Sachin Tendulkar</span>
         </Fade>
         <br />
         <Fade bottom delay={8000}>
-          <span className="pd-700 name">Tendulkar</span>
+          <p
+            className="ssp-300"
+            style={{
+              color: "white"
+            }}
+          >
+            Lorem ipsum is placeholder text commonly used in the graphic, print,
+            and publishing industries for previewing layouts and visual mockups.
+          </p>
+          <button className="ssp-400 insights-button">
+            See career insights
+          </button>
         </Fade>
       </div>
     </section>
